4f68b0a5eb06b76afdcf37d3a24a1c7ed4365535
Andrei Ilie Pîrvan <pirvanandrei@ymail.com>
2020-07-10 16:25:31 +0200

    adding the build to backend

---
+++ build/precache-manifest.21bcee7d5c16f6ff4a8f0a49ba58d9e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1e20fd279827f839d8380b180a56f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "826eacc0551f71482cdb",
+    "url": "/static/css/main.6b1fcfc4.chunk.css"
+  },
+  {
+    "revision": "a212f0b827a0e9088781",
+    "url": "/static/js/2.f13c1df0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f13c1df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "826eacc0551f71482cdb",
+    "url": "/static/js/main.10f97e50.chunk.js"
+  },
+  {
+    "revision": "6c098f24d5779e6b8b45",
+    "url": "/static/js/runtime-main.7e830e89.js"
+  }
+]);
