00a53736834aa475da9bff6e6273d3aee15cd1a3
Andrei Ilie Pîrvan <pirvanandrei@ymail.com>
2020-07-27 10:26:29 +0200

    chaged app file structure, added new logger.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const app = require('./app') // the actual Express application
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
+
+
+
+
+
+
+
+
 require('dotenv').config();
 const express = require('express');
 const app = express();
