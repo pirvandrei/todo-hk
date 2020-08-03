55c844a1aabbfdb81e35bea65f779c6343475b92
Andrei Ilie Pîrvan <pirvanandrei@ymail.com>
2020-08-03 19:45:58 +0200

    initializing the database before tests

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -3,6 +3,30 @@ const supertest = require('supertest');
 const app = require('../app');
 
 const api = supertest(app);
+const Note = require('../models/note');
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+    date: new Date(),
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    important: true,
+    date: new Date(),
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
 
 test('notes are returned as json', async () => {
   await api
@@ -16,13 +40,15 @@ test('there are two notes', async () => {
 
   // execution gets here only after the HTTP request is complete
   // the result of HTTP request is saved in variable res
-  expect(response.body).toHaveLength(3);
+  expect(response.body).toHaveLength(initialNotes.length);
 });
 
 test('the first note is about HTTP methods', async () => {
   const response = await api.get('/api/notes');
 
-  expect(response.body[0].content).toBe('HTML is Easy');
+  const contents = response.body.map(r => r.content);
+  console.log(contents);
+  expect(contents).toContain('Browser can execute only Javascript');
 });
 
 afterAll(() => {
