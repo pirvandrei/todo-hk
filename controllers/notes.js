4547246a99168075790076067ae847d908fd3723
Andrei Ilie Pîrvan <pirvanandrei@ymail.com>
2020-08-04 10:34:17 +0200

    Create a new note and puluate users/notes

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,9 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
+const User = require('../models/user');
 
 notesRouter.get('/',  (req, res) => {
-  Note.find({})
+  Note.find({}).populate('user', { username: 1, name: 1 })
     .then((notes) => {
       res.json(notes);
     })
@@ -47,6 +48,8 @@ notesRouter.put('/:id', (request, response, next) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body;
 
+  const user = await User.findById(body.userId);
+
   if (body.content === undefined) {
     return response.status(400).json({
       error: 'content missing',
@@ -57,9 +60,14 @@ notesRouter.post('/', async (request, response) => {
     content: body.content,
     important: body.important || false,
     date: new Date(),
+    user: user._id
   });
 
   const savedNote = await note.save();
+
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   response.json(savedNote);
 });
 
