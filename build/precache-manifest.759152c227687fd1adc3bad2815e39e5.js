063ff3790d790e12412ec09dd782d4edca733805
Andrei Ilie Pîrvan <pirvanandrei@ymail.com>
2020-08-08 11:00:16 +0200

    handle login on fronend

--- build/precache-manifest.759152c227687fd1adc3bad2815e39e5.js
+++ build/precache-manifest.759152c227687fd1adc3bad2815e39e5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c426ca6d5513c5a7dfc78412c837422",
+    "revision": "80a90caa3cf1e83aaa2da87dd693691e",
     "url": "/index.html"
   },
   {
-    "revision": "b0e863ab32fc09c6c2b7",
+    "revision": "046ea4b2449d7e9447a6",
     "url": "/static/css/main.6b1fcfc4.chunk.css"
   },
   {
-    "revision": "a212f0b827a0e9088781",
-    "url": "/static/js/2.f13c1df0.chunk.js"
+    "revision": "57b1e43c1087e1b37a34",
+    "url": "/static/js/2.aa344d5a.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.f13c1df0.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.aa344d5a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b0e863ab32fc09c6c2b7",
-    "url": "/static/js/main.87368a81.chunk.js"
+    "revision": "046ea4b2449d7e9447a6",
+    "url": "/static/js/main.5d57488b.chunk.js"
   },
   {
     "revision": "6c098f24d5779e6b8b45",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.68a06c63bb345e7a22e83927ca9c3333.js"
+  "/precache-manifest.759152c227687fd1adc3bad2815e39e5.js"
 );
 
 self.addEventListener('message', (event) => {
